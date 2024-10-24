8739dbe3d559ea23a5c303a796f8a436f40fb36b
abdallahawad3 <djdgdnnxcbj@gmail.com>
2024-10-25 00:55:19 +0300

    feat: Add BrandsPage and Pagination component
    - Add BrandsPage component to display brand cards
    - Add Pagination component for navigating through pages

--- src/Pages/Brands/Brands.tsx
+++ src/Pages/Brands/Brands.tsx
@@ -1,5 +1,31 @@
+import BrandCard from "../../components/Brands/BrandCard";
+import Pagination from "../../components/utils/Pagination";
+
 const BrandsPage = () => {
-  return <div>Brands</div>;
+  return (
+    <main className="py-10">
+      <div className="container mb-7 grid justify-center gap-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        <BrandCard />
+        <BrandCard />
+        <BrandCard />
+        <BrandCard />
+        <BrandCard />
+        <BrandCard />
+        <BrandCard />
+        <BrandCard />
+        <BrandCard />
+        <BrandCard />
+        <BrandCard />
+        <BrandCard />
+        <BrandCard />
+        <BrandCard />
+        <BrandCard />
+      </div>
+      <div className="text-center">
+        <Pagination />
+      </div>
+    </main>
+  );
 };
 
 export default BrandsPage;
