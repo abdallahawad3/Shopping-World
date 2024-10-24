da363f9c1e8c137758f7bb068098b7aaf9100bfc
abdallahawad3 <djdgdnnxcbj@gmail.com>
2024-10-25 01:00:13 +0300

    feat: Add CategoryPage with CategoryCard and Pagination components

--- src/Pages/Catrgories/Category.tsx
+++ src/Pages/Catrgories/Category.tsx
@@ -1,5 +1,28 @@
+import CategoryCard from "../../components/Category/CategoryCard";
+import Pagination from "../../components/utils/Pagination";
+
 const CategoryPage = () => {
-  return <div>Category</div>;
+  return (
+    <main className="my-10">
+      <div className="container mb-20 grid justify-center gap-2 md:grid-cols-3 md:gap-y-10 lg:grid-cols-5 xl:grid-cols-6">
+        <CategoryCard />
+        <CategoryCard />
+        <CategoryCard />
+        <CategoryCard />
+        <CategoryCard />
+        <CategoryCard />
+        <CategoryCard />
+        <CategoryCard />
+        <CategoryCard />
+        <CategoryCard />
+        <CategoryCard />
+        <CategoryCard />
+      </div>
+      <div className="text-center">
+        <Pagination />
+      </div>
+    </main>
+  );
 };
 
 export default CategoryPage;
