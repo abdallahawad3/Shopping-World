c7eaa26c3ec385f101517d11a3ae19265406459d
abdallahawad3 <djdgdnnxcbj@gmail.com>
2024-12-06 19:52:16 +0200

    Enhance CartSidebar: add coupon functionality with input handling and update price calculations

--- src/Pages/Cart/CartSidebar.tsx
+++ src/Pages/Cart/CartSidebar.tsx
@@ -1,19 +1,35 @@
 import { useSelector } from "react-redux";
 import { useAppDispatch, type RootState } from "../../app/store";
-import { useEffect } from "react";
-import { getAllCartProducts } from "../../app/feature/Cart/cartSlice";
+import { useEffect, useState } from "react";
+import {
+  applyCoupon,
+  getAllCartProducts,
+} from "../../app/feature/Cart/cartSlice";
 import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const CartSidebar = () => {
-  const { totalCartPrice, discount } = useSelector(
+  const [coupon, setCoupon] = useState("");
+
+  const { totalCartPrice, priceAfterDiscount } = useSelector(
     (state: RootState) => state.cart,
   );
 
   const dispatch = useAppDispatch();
 
+  const handelCoupon = () => {
+    if (coupon.length != 0) {
+      dispatch(applyCoupon(coupon));
+    } else {
+      toast.error("Enter the coupon code", {
+        position: "top-right",
+      });
+    }
+  };
   useEffect(() => {
     dispatch(getAllCartProducts());
   }, [dispatch]);
+
   return (
     <div>
       <div className="m-[30px] mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full">
@@ -29,22 +45,27 @@ const CartSidebar = () => {
                   Original price
                 </dt>
                 <dd className="text-base font-medium text-gray-900 dark:text-white">
-                  ${totalCartPrice}
+                  {Math.ceil(totalCartPrice)}$
                 </dd>
               </dl>
               <dl className="flex items-center justify-between gap-4">
                 <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
-                  Discount
+                  After discount
                 </dt>
                 <dd className="text-base font-medium text-gray-900 dark:text-white">
-                  {discount}%
+                  {Math.ceil(priceAfterDiscount)}$
                 </dd>
               </dl>
               <dl className="flex items-center justify-between gap-4">
                 <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
                   Savings
                 </dt>
-                <dd className="text-base font-medium text-green-600">-${0}</dd>
+                <dd className="text-base font-medium text-green-600">
+                  -$
+                  {priceAfterDiscount > 0
+                    ? Math.ceil(totalCartPrice - priceAfterDiscount)
+                    : 0}
+                </dd>
               </dl>
             </div>
             <dl className="flex items-center justify-between gap-4 border-t border-gray-200 pt-2 dark:border-gray-700">
@@ -52,7 +73,10 @@ const CartSidebar = () => {
                 Total
               </dt>
               <dd className="text-base font-bold text-gray-900 dark:text-white">
-                ${totalCartPrice}
+                $
+                {priceAfterDiscount > 0
+                  ? Math.ceil(priceAfterDiscount)
+                  : Math.ceil(totalCartPrice)}
               </dd>
             </dl>
           </div>
@@ -95,7 +119,13 @@ const CartSidebar = () => {
         </div>
 
         <div className="space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm sm:p-6 dark:border-gray-700 dark:bg-gray-800">
-          <form className="space-y-4">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handelCoupon();
+            }}
+            className="space-y-4"
+          >
             <div>
               <label
                 htmlFor="voucher"
@@ -108,8 +138,10 @@ const CartSidebar = () => {
                 type="text"
                 id="voucher"
                 className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
-                placeholder=""
-                required
+                value={coupon}
+                onChange={(e) => {
+                  setCoupon(e.target.value);
+                }}
               />
             </div>
             <button
