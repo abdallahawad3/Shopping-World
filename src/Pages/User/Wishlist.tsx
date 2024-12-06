fd6a79959abcdf056eda426e3513c1d46e9cc5ff
abdallahawad3 <djdgdnnxcbj@gmail.com>
2024-12-06 19:53:26 +0200

    Implement wishlist functionality in WishlistPage: display products from the user's wishlist

--- src/Pages/User/Wishlist.tsx
+++ src/Pages/User/Wishlist.tsx
@@ -1,7 +1,18 @@
+import { useSelector } from "react-redux";
+import type { RootState } from "../../app/store";
+import ProductCard from "../../components/utils/ProductCard";
+
 const WishlistPage = () => {
+  const { wishlistProducts } = useSelector(
+    (state: RootState) => state.wishlist,
+  );
+
   return (
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 ">
-      WishlistPage
+      {wishlistProducts &&
+        wishlistProducts.map((ele) => (
+          <ProductCard product={ele} key={ele._id} />
+        ))}
     </div>
   );
 };
